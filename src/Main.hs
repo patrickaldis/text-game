84fad5fbe22c51048799eb727e7018305394bd09
Patrick Aldis <patricktaldis@gmail.com>
2026-04-10 12:07:06 +0100

    current

--- src/Main.hs
+++ src/Main.hs
@@ -8,11 +8,11 @@
 module Main (main) where
 
 import Control.Monad.Fix (MonadFix)
+import Data.Functor (void)
 import Data.Text qualified as T
-import Game.Types hiding (nextState, text)
-import Graphics.Vty qualified as V
+import Game.Types hiding (nextState)
+import Graphics.Vty.Input qualified as V
 import Reflex
-import Reflex.Network (networkView)
 import Reflex.Vty
 import Prelude hiding (sequence)
 
@@ -35,59 +35,40 @@ app ::
   , Adjustable t m
   , PostBuild t m
   , NotReady t m
+  , MonadHold t m
   ) =>
   m (Event t ())
 app = do
+  advance <- keyCombo (V.KEnter, []) >>= f . void
 
   col do
     tile flex $
       text "picture here"
 
     tile (fixed 6) $
-      drawSequence ChatState {s0=StateStart, a=StartAction, i=0}
+      drawCurrentFrame advance
 
--- drawSequence :: (Monad m, HasInput t m, Reflex t) => ChatState s s' -> m (Event t ())
-drawSequence ::
-  ( Reflex t
-  , MonadHold t m
-  , MonadFix m
-  , HasInput t m
-  , HasTheme t m
-  , HasDisplayRegion t m
-  , HasImageWriter t m
-  , HasFocusReader t m
-  , HasFocus t m
-  , HasLayout t m
-  , Adjustable t m
-  , PostBuild t m
-  , NotReady t m
-  ) =>
-  ChatState Start S1 -> m (Event t ())
-drawSequence s = do
-  case transition (s0 s) (a s) of
-    (StateS1, Sequence seq') -> do
-      -- Read input at the app level
-      ev <- input
-      let qPressed = fforMaybe ev $ \case
-            V.EvKey (V.KChar 'q') [] -> Just ()
-            _ -> Nothing
-      drawFrame $ seq' !! i s
-      case nextState s of
-        Left e -> _
-        Right r -> _
-      pure qPressed
+    ctrlc
+
+f :: (Reflex t, MonadHold t m, MonadFix m) => Event t () -> m (Dynamic t (ChatState s s'))
+f e = foldDyn func s0 e
+ where
+  s0 = ChatState StateStart StartAction 0
+  func _ s = case nextFrame s of
+    Left s' -> s'
+    Right _ -> error "unimplemented"
 
 transition :: State s -> Action s s' -> (State s', Sequence)
 transition = \case
   StateStart -> \case
     StartAction ->
-      (StateS1,
-        Sequence
+      ( StateS1
+      , Sequence
           [ Frame P1 "Hi here's text 1"
           , Frame P2 "Now I'm saying something"
           , Frame P1 "I stink"
           ]
-        )
+      )
   StateS1 -> \case
     SayHi -> (StateS1, Sequence [])
     GoAway -> (StateS1, Sequence [])
@@ -98,11 +79,19 @@ data ChatState s s' = ChatState
   , i :: Int
   }
 
-nextState :: ChatState s s' -> Either (ChatState s s') (State s')
-nextState state | i state == length y = Right x
-  where
-    (x, Sequence y) = transition (s0 state) (a state)
-nextState state = Left state{i = i state + 1}
+getFrame :: ChatState s s' -> Frame
+getFrame (ChatState s a i) =
+  case snd . transition s $ a of
+    Sequence fs -> fs !! i
+
+nextFrame :: ChatState s s' -> Either (ChatState s s') (State s')
+nextFrame state =
+  if i state /= length y
+    then Left state{i = i'}
+    else Right x
+ where
+  (x, Sequence y) = transition (s0 state) (a state)
+  i' = i state + 1
 
 drawFrame ::
   ( Reflex t
@@ -121,3 +110,17 @@ drawFrame (Frame c t) =
     (constant thickBoxStyle)
     (constant . T.pack $ name c)
     (text . constant . T.pack $ t)
+
+drawCurrentFrame ::
+  ( Reflex t
+  , MonadHold t m
+  , MonadFix m
+  , HasTheme t m
+  , HasDisplayRegion t m
+  , HasImageWriter t m
+  , HasFocusReader t m
+  , HasInput t m
+  ) =>
+  Dynamic t (ChatState s s') ->
+  m ()
+drawCurrentFrame state = void $ forDynM state (drawFrame . getFrame)
